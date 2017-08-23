46aaaf1f99376c91f2dadedcf06a118f416a3db5
Vincent Masiello <vincentmasiello@gmail.com>
2017-08-23 18:00:25 -0400

    write RunGuest, add infinite loop to host and guest, supply more info at runtime

--- lib/main.go
+++ lib/main.go
@@ -5,13 +5,25 @@ import (
     "log"
     "net"
     "bufio"
+    "os"
 )
+
 const port = "8080"
+
 // RunHost takes an ip as argument
 // and listens for connections on that ip
 func RunGuest(ip string) {
-	fmt.Println("look, I'm using fmt!")
+    ipAndPort := ip + ":" + port
+    conn, dialErr := net.Dial("tcp", ipAndPort)
+    if dialErr != nil {
+        log.Fatal("Error: ", dialErr)
+    }
+
+    for {
+        handleGuest(conn)
+    }
 }
+
 // RunGuest takes destination ip as argument
 // and connects to that ip
 func RunHost(ip string) {
@@ -20,12 +32,21 @@ func RunHost(ip string) {
 	if listenErr != nil {
 		log.Fatal("Error: ", listenErr)
 	}
+    fmt.Println("Listening on ", ipAndPort)
 
 	conn, acceptErr := listener.Accept()
 
 	if acceptErr != nil {
 		log.Fatal("Error: ", acceptErr)
 	}
+    fmt.Println("New Connection Accepted")
+
+    for {
+        handleHost(conn)
+    }
+}
+
+func handleHost(conn net.Conn) {
 	// read the message from the connection
 	reader := bufio.NewReader(conn)
 	message, readErr := reader.ReadString('\n') // accepts a delimiter for end of message (copy loud and clear, OVER)
@@ -34,4 +55,33 @@ func RunHost(ip string) {
 	}
 
 	fmt.Println("Message received: " + message)
+    fmt.Print("SendMessage: ")
+
+    replyReader := bufio.NewReader(os.Stdin)
+    replyMessage, replyErr := replyReader.ReadString('\n')
+    if replyErr != nil {
+        log.Fatal("Error: ", replyMessage)
+    }
+
+    fmt.Fprint(conn, replyMessage)
+}
+
+func handleGuest(conn net.Conn) {
+    
+    fmt.Print("Send Message: ")
+
+    reader := bufio.NewReader(os.Stdin)
+    message, readErr := reader.ReadString('\n')
+    if readErr != nil {
+        log.Fatal("Error: ", readErr)
+    }
+
+    fmt.Fprint(conn, message)
+
+    replyReader := bufio.NewReader(conn)
+    replyMessage, replyErr := replyReader.ReadString('\n')
+    if replyErr != nil {
+        log.Fatal("Error: ", replyErr)
+    }
+    fmt.Println("Message received: ", replyMessage)
 }
